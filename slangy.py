8af94596a138dae847922c9d24a1b3385ca9e0a1
Rishab Kumar <45825464+rishabkumar7@users.noreply.github.com>
2022-09-10 11:57:59 -0400

    initial commit

---
+++ slangy.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+import requests
+import re
+import os
+from twilio.twiml.messaging_response import MessagingResponse
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    print (incoming_msg)
+    filter_msg= re.findall('\(.*?\)', incoming_msg)
+    string_without_parentheses = re.sub(r"[\(\)]",'',filter_msg[0])
+    term = string_without_parentheses
+    print (term)
+    resp = MessagingResponse()
+    msg = resp.message()
+    
+    url = "https://urban-dictionary7.p.rapidapi.com/v0/define"
+    querystring = {"term":f'"{term}"'}
+    headers = {
+	            "X-RapidAPI-Key": os.getenv('APIKEY'),
+	    "X-RapidAPI-Host": "urban-dictionary7.p.rapidapi.com"
+    }
+    responded = False
+    if 'quote' in incoming_msg:
+        # return a quote
+        r = requests.request("GET", url, headers=headers, params=querystring)
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["list"][0]["definition"]}' #({data["example"]})'
+        else:
+            quote = 'Sorry try again.'
+        msg.body(quote)
+        responded = True
+    if not responded:
+        msg.body('Sorry, didnt get that')
+    return str(resp)
+
+
+if __name__ == '__main__':
+    app.run(port=6000)
